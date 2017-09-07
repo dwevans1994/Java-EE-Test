79cdb8703ad4895bc66485ea72ea4fd882d97c25
Dylan <st20039680@outlook.cardiffmet.ac.uk>
2017-09-07 11:03:31 +0000

    java test red

---
+++ JavaEEtest/bookGenreTest.java
@@ -0,0 +1,13 @@
+package JavaEEtest;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class bookGenreTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
